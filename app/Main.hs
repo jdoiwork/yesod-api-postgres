49b90e3cdd658fd3fbc08cac6f1ac3fe61a0d6dd
Junichiro Doi <jdoi.work@gmail.com>
2023-10-19 09:18:01 +0900

    Yesod追加

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main (main) where
 
 import Lib
+-- import Yesod
 
 main :: IO ()
-main = someFunc
+main = runApp 3000
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,25 @@
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE TemplateHaskell       #-}
+{-# LANGUAGE TypeFamilies          #-}
 module Lib
-    ( someFunc
+    ( HelloWorld(..)
+    , runApp
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+
+import           Yesod
+
+data HelloWorld = HelloWorld
+
+mkYesod "HelloWorld" [parseRoutes|
+/ HomeR GET
+|]
+
+instance Yesod HelloWorld
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout [whamlet|Hello World!|]
+
+runApp :: Int -> IO ()
+runApp port = warp port HelloWorld
